f08e01c70c69250befdaa83db764f3dc7ae72260
InvisibleOS <shivanshsrivastava121@gmail.com>
2026-04-07 07:46:08 +0530

    Bumped versionCode to 2 for Obtainium update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,9 @@ android {
         applicationId = "com.shivansh.waketracker"
         minSdk = 34
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
