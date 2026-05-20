496c697ead15c98d5284e9d8c52a6e213d596206
InvisibleOS <shivanshsrivastava121@gmail.com>
2026-05-20 17:16:58 +0530

    Update versionCode to 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 34
         targetSdk = 35
 
-        versionCode = 5
+        versionCode = 7
         versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
